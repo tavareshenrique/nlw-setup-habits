852aad9216ba84b441ae729c9c5676fd30d03d58
Henrique Tavares <ihenrits@gmail.com>
2023-01-16 21:16:10 -0300

    starting web

---
+++ web/src/App.tsx
@@ -0,0 +1,5 @@
+import { Habit } from "./components/Habit";
+
+export function App() {
+  return <Habit completed />
+}
---
+++ web/src/components/Habit.tsx
@@ -0,0 +1,9 @@
+interface IHabitsProps {
+  completed: boolean;
+}
+
+export function Habit({ completed }: IHabitsProps) {
+  return (
+    <h1>Habit</h1>
+  );
+}
