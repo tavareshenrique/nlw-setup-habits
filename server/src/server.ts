04502eeeb1ebcd10ec1fbcf8bb7e9a76952e57d7
Henrique Tavares <ihenrits@gmail.com>
2023-01-17 21:21:38 -0300

    create new routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,15 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
 
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from "./routes"
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
-app.register(cors, {
-  origin: '*',
-})
-
-app.get('/hello', async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: 'Beber'
-      }
-    }
-  })
-
-  return habits
-})
+app.register(cors)
+app.register(appRoutes)
 
 app.listen({
   port: 3333,
 }).then(() => {
-  console.log('HTTP Server is running!')
+  console.log('HTTP Server running!')
 })
